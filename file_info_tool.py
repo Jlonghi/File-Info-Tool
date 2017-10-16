15cb8327cb10bdc6379166a98c100413579fe6b5
josh0588 <joshualonghi@gmail.com>
2017-10-16 12:13:12 -0400

    added md5 hash function

--- file_info_tool.py
+++ file_info_tool.py
@@ -1,6 +1,15 @@
 
 #!/usr/bin/env python
-
+import hashlib
 
 
 # File Info Tool 
+def md5File(filePath):
+    try:
+        file = open(filePath)
+        hash = hashlib.md5()
+        hash.update(file.read())
+        return hash.hexdigest()
+    except IOError:
+        print('Could not find file')
+print (md5File('C:\Users\joshu\DdsModel2.log'))
